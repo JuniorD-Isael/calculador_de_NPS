eb5699d701a8ef9b819c3e06f9990eb4adbd95f3
JuniorD-Isael <104312466+JuniorD-Isael@users.noreply.github.com>
2022-10-16 23:04:00 -0300

    Criada a migrations de users close #2

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,10 @@
 import "reflect-metadata"
 import express from 'express';
 import "./database";
+import {router} from "./routes";
 
 const app = express();
-
-
-/**
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração especifica
- */
-
-
-// http://localhost:3333
-app.get("/", (request, response) => {
-
-    return response.json({message: "Olá, Mundo"});
-});
-
-// 1 param => Rota(Recurso API)
-// 2 param => request, responce
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"})
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
